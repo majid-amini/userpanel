c8aadc3e54f4940e3c6be8f0a81b91dadcb4972f
majidamini <majidamini_s@yahoo.com>
2023-07-03 13:53:00 +0330

    new styles such as fontsize progressbar and responsive codes added

---
+++ progress.js
@@ -0,0 +1,30 @@
+var circle = new ProgressBar.Circle("#progress", {
+  color: "#000", // Set the color of the progress bar
+  strokeWidth: 8, // Set the width of the progress bar stroke
+  duration: 2000, // Set the animation duration in milliseconds
+  easing: "easeInOut", // Set the easing function for the animation
+});
+circle.animate(0.3); // Replace 0.7 with your desired progress value
+
+var circle = new ProgressBar.Circle("#progress-2", {
+  color: "#000",
+  strokeWidth: 8,
+  duration: 2000,
+  easing: "easeInOut",
+});
+circle.animate(0.55);
+// Animate the progress bar to a specific value (0.0 to 1.0)
+var circle = new ProgressBar.Circle("#progress-3", {
+  color: "#000",
+  strokeWidth: 8,
+  duration: 2000,
+  easing: "easeInOut",
+});
+circle.animate(0.7);
+var circle = new ProgressBar.Circle("#progress-4", {
+  color: "#000",
+  strokeWidth: 8,
+  duration: 2000,
+  easing: "easeInOut",
+});
+circle.animate(0.8);
