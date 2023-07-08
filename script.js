13336c9d72d048ea200da955edf8578917fc201a
majidamini <majidamini_s@yahoo.com>
2023-07-08 16:13:08 +0330

    support section added

--- script.js
+++ script.js
@@ -30,11 +30,6 @@ $(document).ready(function () {
     }
   });
 
-  // if ($("#modal__dark").attr(style) === 'display: block;') {
-  //   console.log("yes");
-  // }
-  console.log($("#modal__dark").attr);
-
   $(".menu-icon").click(function () {
     $(this).toggleClass("on");
   });
@@ -42,6 +37,28 @@ $(document).ready(function () {
   $(".name__edit__icon").click(function () {
     $(".modal__name").removeClass("d-none");
     $("#modal__dark").css("display", "block");
+  });
+
+  $(".edit__icon").click(function () {
+    var scrollPosition = [
+      self.pageXOffset ||
+        document.documentElement.scrollLeft ||
+        document.body.scrollLeft,
+      self.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop,
+    ];
+    var html = jQuery("html"); // it would make more sense to apply this to body, but IE7 won't have that
+    html.data("scroll-position", scrollPosition);
+    html.data("previous-overflow", html.css("overflow"));
+    html.css("overflow", "hidden");
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+  });
+  $(".delete__icon").click(function () {
+    var html = jQuery("html");
+    var scrollPosition = html.data("scroll-position");
+    html.css("overflow", html.data("previous-overflow"));
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
   });
   $(".fa-circle-xmark").click(function () {
     $(".modal__name").addClass("d-none");
